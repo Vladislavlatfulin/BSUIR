e35cb8b6ae6b3499ec9dcb6ac5dda621f97db9ed
Vlad Latfulin <81932668+Vladislavlatfulin@users.noreply.github.com>
2022-02-27 23:55:49 +0300

    gg

---
+++ 4 semester/apk/ports.py
@@ -0,0 +1,26 @@
+from traceback import print_tb
+import serial
+
+
+def serialPorts():
+    port1 = serial.Serial('/dev/ttys000')
+    port2 = serial.Serial('/dev/ttys002')
+
+    print("Port1 is open: " + str(port1.isOpen()))
+    print("Port2 is open: " + str(port2.isOpen()))
+
+    port1.write(b'hello')
+    print(port1.inWaiting())
+
+    data = port2.read_all()
+
+    print(data)
+
+    port1.close()
+    print("Port1 is closed")
+    port2.close()
+    print("Port2 is closed")
+
+
+if __name__ == '__main__':
+    serialPorts()
